9788ab2a9f94ac04e693ab3f78f0854914b4f1f4
HamidP2M <mdhamidhosen8444@gmail.com>
2025-04-16 15:25:26 +0600

    Products Design

---
+++ EasyShop/app/src/main/java/com/hamidhosen/easyshop/model/ProductModel.kt
@@ -0,0 +1,11 @@
+package com.hamidhosen.easyshop.model
+
+data class ProductModel(
+    val id: String = "",
+    val title: String = "",
+    val description:  String = "",
+    val price: String = "",
+    val actualPrice: String = "",
+    val category: String = "",
+    val images: List<String> = emptyList()
+)
--- EasyShop/app/src/main/java/com/hamidhosen/easyshop/pages/CategoryProductsPage.kt
+++ EasyShop/app/src/main/java/com/hamidhosen/easyshop/pages/CategoryProductsPage.kt
@@ -1,11 +1,47 @@
 package com.hamidhosen.easyshop.pages
 
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
+import androidx.compose.ui.unit.dp
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.hamidhosen.easyshop.components.ProductItemView
+import com.hamidhosen.easyshop.model.ProductModel
 
 @Composable
 fun CategoryProductPage(modifier: Modifier = Modifier, categoryId: String) {
-    Text("Category Product page :::::"+categoryId)
+    var productList by remember { mutableStateOf<List<ProductModel>>(emptyList()) }
+
+    LaunchedEffect(categoryId) {
+        Firebase.firestore.collection("data").document("stock")
+            .collection("products")
+            .whereEqualTo("category", categoryId)
+            .get()
+            .addOnSuccessListener { result ->
+                val resultList = result.documents.mapNotNull { doc ->
+                    doc.toObject(ProductModel::class.java)
+                }
+                productList = resultList
+            }
+    }
+
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        items(productList.chunked(2)) { rowItem ->
+            Row {
+                rowItem.forEach {
+                    ProductItemView(product = it, modifier = Modifier.weight(1f))
+                }
+                if (rowItem.size == 1) {
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+            }
+        }
+    }
 }
